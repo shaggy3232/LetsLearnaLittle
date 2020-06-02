a803557bc06525eb5fe25b200510624b01a6c87e
shaggy3232 <sbrem044@uottawa.ca>
2020-06-02 13:29:06 -0400

    init

---
+++ LetsLearnALittle/LetsLearnALittle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsLearnALittle
+//
+//  Created by Shaggy Bremnath on 2020-06-02.
+//  Copyright © 2020 Shaggy Bremnath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
