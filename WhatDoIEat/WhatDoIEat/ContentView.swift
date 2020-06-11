71fa6d07e01f1b4d7c274429576654cf8a53e1f5
shaggy3232 <sbrem044@uottawa.ca>
2020-06-11 12:13:04 -0400

    location stored

--- WhatDoIEat/WhatDoIEat/ContentView.swift
+++ WhatDoIEat/WhatDoIEat/ContentView.swift
@@ -7,15 +7,68 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @ObservedObject var obs = obserever()
+    @ObservedObject var locationManager = LocationManager()
+    
+    
+    
+ 
     var body: some View {
-        Text("Hello, World!")
+       
+        let coordinate = self.locationManager.location != nil ?
+            self.locationManager.location!.coordinate :CLLocationCoordinate2D()
+        
+        
+        return VStack(alignment: .center, spacing: 20) {
+            ZStack{
+                
+                VStack{
+                     Text("What do you want to eat?")
+                    Text("\(coordinate.latitude), \(coordinate.longitude)")
+                    
+   
+                }
+            }
+            
+            
+            
+        }
     }
 }
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+class obserever : ObservableObject{
+    
+    @Published var Rests = [restaurant]()
+    
+    init(){
+        let url = "https://developers.zomato.com/api/v2.1/geocode?lat=\(coordinate.latitude)&lon=\(coordinate.longitude)"
+        let api = "7f99f4022b4612cf1711ebfd5198d544"
+    }
+    
+}
+
+struct restaurant : Identifiable{
+    var id:String
+    var name : String
+    var image : String
+    var rating : String
+    var webUrl : String
+}
+
+
